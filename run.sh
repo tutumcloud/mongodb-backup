c4d7e56fdf5a50a4f8ed1861204484937cbd851c
tifayuki <tifayuki@gmail.com>
2015-08-07 18:45:43 +0200

    remove backups only on when new backup succeeds

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "${MONGODB_ENV_MONGODB_PASS}" == "**None**" ]; then
-        unset MONGODB_ENV_MONGODB_PASS
-fi
-
 MONGODB_HOST=${MONGODB_PORT_27017_TCP_ADDR:-${MONGODB_HOST}}
 MONGODB_HOST=${MONGODB_PORT_1_27017_TCP_ADDR:-${MONGODB_HOST}}
 MONGODB_PORT=${MONGODB_PORT_27017_TCP_PORT:-${MONGODB_PORT}}
@@ -17,25 +13,31 @@ MONGODB_PASS=${MONGODB_PASS:-${MONGODB_ENV_MONGODB_PASS}}
 [[ ( -n "${MONGODB_PASS}" ) ]] && PASS_STR=" --password ${MONGODB_PASS}"
 [[ ( -n "${MONGODB_DB}" ) ]] && USER_STR=" --db ${MONGODB_DB}"
 
-BACKUP_CMD="mongodump --out /backup/"'$(date +\%Y.\%m.\%d.\%H\%M\%S)'" --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
-
+BACKUP_CMD="mongodump --out /backup/"'${BACKUP_NAME}'" --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR}${DB_STR} ${EXTRA_OPTS}"
 
 echo "=> Creating backup script"
 rm -f /backup.sh
 cat <<EOF >> /backup.sh
 #!/bin/bash
 MAX_BACKUPS=${MAX_BACKUPS}
+BACKUP_NAME=\$(date +\%Y.\%m.\%d.\%H\%M\%S)
 
 echo "=> Backup started"
+if ${BACKUP_CMD} ;then
+    echo "   Backup succeeded"
+else
+    echo "   Backup failed"
+    rm -rf /backup/\${BACKUP_NAME}
+fi
+
 if [ -n "\${MAX_BACKUPS}" ]; then
-    while [ \$(ls /backup -N1 | wc -l) -ge \${MAX_BACKUPS} ];
+    while [ \$(ls /backup -N1 | wc -l) -gt \${MAX_BACKUPS} ];
     do
         BACKUP_TO_BE_DELETED=\$(ls /backup -N1 | sort | head -n 1)
-        echo "Deleting backup \${BACKUP_TO_BE_DELETED}"
+        echo "   Deleting backup \${BACKUP_TO_BE_DELETED}"
         rm -rf /backup/\${BACKUP_TO_BE_DELETED}
     done
 fi
-${BACKUP_CMD}
 echo "=> Backup done"
 EOF
 chmod +x /backup.sh
@@ -45,7 +47,11 @@ rm -f /restore.sh
 cat <<EOF >> /restore.sh
 #!/bin/bash
 echo "=> Restore database from \$1"
-mongorestore --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR} \$1
+if mongorestore --host ${MONGODB_HOST} --port ${MONGODB_PORT} ${USER_STR}${PASS_STR} \$1; then
+    echo "   Restore succeeded"
+else
+    echo "   Restore failed"
+fi
 echo "=> Done"
 EOF
 chmod +x /restore.sh
@@ -54,12 +60,11 @@ touch /mongo_backup.log
 tail -F /mongo_backup.log &
 
 if [ -n "${INIT_BACKUP}" ]; then
-    echo "=> Create a backup on start the container"
+    echo "=> Create a backup on the startup"
     /backup.sh
 fi
 
 echo "${CRON_TIME} /backup.sh >> /mongo_backup.log 2>&1" > /crontab.conf
 crontab  /crontab.conf
-crontab -l
 echo "=> Running cron job"
 exec cron -f
